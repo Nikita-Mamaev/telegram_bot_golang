d7c395f4fdd31c82be18221305352dbd132d6a86
nikitamamaev <nikratos@yandex.ru>
2021-05-04 23:14:37 +0300

    feat: init commit

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/Nikita-Mamaev/telegram_bot_golang/pkg/telegram"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("1713704434:AAFT8OFoy4xYBYKARwuHSEP4pT9X8h7xCpk")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+	telegramBot.Start()
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Nikita-Mamaev/telegram_bot_golang.git
+
+go 1.16
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
